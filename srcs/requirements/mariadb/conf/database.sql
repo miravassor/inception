fe0f4c2bb1a6f4cf4bafac2481b92387b963018c
avassor <avassor@student.42.fr>
2023-11-02 16:02:29 +0100

    2

--- srcs/requirements/mariadb/conf/database.sql
+++ srcs/requirements/mariadb/conf/database.sql
@@ -1,6 +1,6 @@
 USE mysql;
 FLUSH PRIVILEGES;
-DELETE FROM     mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
